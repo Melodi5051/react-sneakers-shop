f4d6ffb4722c6f59aa3875d06a4a10930edd51e3
Artem Natochin <melodi1337@mail.ru>
2022-08-31 18:11:20 +0300

    работа с версткой

--- src/App.js
+++ src/App.js
@@ -1,58 +1,28 @@
-
-export default function App() {
+import Card from "./components/Card";
+import Header from "./components/Header";
+import Drawer from "./components/Drawer";
+function App() {
   return (
     <div className="wrapper clear">
-      <header className={"d-flex justify-between align-center p-40"}>
-        <div className={"d-flex align-center"}>
-            <img width={40} height={40} src="/img/logo.png" alt=""/>
-          <div>
-            <h3 className={"text-uppercase"}>React Sneakers</h3>
-            <p className={'opacity-5'}>Магазин лучших кроссовок</p>
-           </div>
-        </div>
-          <ul className={"d-flex"}>
-            <li className={"mr-30"}>
-                <img width={18} height={18} src="/img/cart.svg" alt=""/>
-                <span>
-                    1205 руб.
-                </span>
-            </li>
-            <li>
-                <img width={18} height={18} src="/img/user.svg" alt=""/>
-            </li>
-          </ul>
-      </header>
+        {/*<Drawer/>*/}
+        <Header/>
       <div className="content p-40">
-        <h1 className={"mb-40"}>Все кроссовки</h1>
+          <div className={"d-flex align-center mb-40 justify-between"}>
+            <h1>Все кроссовки</h1>
+              <div className="search_block d-flex">
+                  <img src="/img/serch.svg" alt="search"/>
+                  <input placeholder="Поиск...."/>
+              </div>
+          </div>
           <div className={'d-flex'}>
-              <div className="card">
-                  <img width={133} height={112} src="/img/sneakers/1.jpg" alt="Sneakers"/>
-                  <h5>Мужские Кроссовки Nike Blazer Mid Suede</h5>
-                  <div className={"d-flex justify-between align-center"}>
-                      <div className={'d-flex flex-column'}>
-                          <span>Цена:</span>
-                          <b>12 999 руб.</b>
-                      </div>
-                      <button className={'button'}>
-                          <img width={30} height={30} src="/img/pluse.svg" alt="Plus"/>
-                      </button>
-                  </div>
-              </div>
-              <div className="card">
-                  <img width={133} height={112} src="/img/sneakers/1.jpg" alt="Sneakers"/>
-                  <h5>Мужские Кроссовки Nike Blazer Mid Suede</h5>
-                  <div className={"d-flex justify-between align-center"}>
-                      <div className={'d-flex flex-column'}>
-                          <span>Цена:</span>
-                          <b>12 999 руб.</b>
-                      </div>
-                      <button className={'button'}>
-                          <img width={30} height={30} src="/img/pluse.svg" alt="Plus"/>
-                      </button>
-                  </div>
-              </div>
+              <Card/>
+              <Card/>
+              <Card/>
+              <Card/>
           </div>
       </div>
     </div>
   );
 }
+
+export default App
---
+++ src/components/Drawer.js
@@ -0,0 +1,39 @@
+function Drawer(){
+    return(
+        <div style={{display:''}} className="overlay">
+            <div className="drawer d-flex flex-column">
+                <h2 className={'mb-30 d-flex justify-between'}>Корзина <img className={'remove_btw cu-p'} src="/img/btw_remove.svg" alt="remove"/></h2>
+                <div className="items">
+                    <div className="cart_item d-flex align-center mb-20">
+                        <div style={{ backgroundImage: 'url(/img/sneakers/1.jpg)'}} className="cart_item_img">
+                        </div>
+                        <div className={'mr-20 flex'}>
+                            <p className={'mb-5'}>Мужские Кроссовки Nike Air Max 270</p>
+                            <b>12 999 руб.</b>
+                        </div>
+                        <img className={'remove_btw'} src="/img/btw_remove.svg" alt="remove"/>
+                    </div>
+                </div>
+                <div className="cart_total_block">
+                    <ul>
+                        <li className={'d-flex align-end'}>
+                            <span>Итого:</span>
+                            <div></div>
+                            <b>21 498 руб. </b>
+                        </li>
+                        <li className={'d-flex align-end'}>
+                            <span>Налог 5%: </span>
+                            <div></div>
+                            <b>1074 руб. </b>
+                        </li>
+                    </ul>
+                    <button className={'green_button'}>Оформить заказ
+                        <img src="/img/arrow.svg" alt="arrow"/>
+                    </button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Drawer
